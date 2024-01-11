d9849016704b834836722a0d3b5d7cd0b1dfe8a1
AnjasFedo <fedoaf98@gmail.com>
2024-01-11 22:40:47 +0700

    Initialize Movie structure

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"log"
-	"encoding/json"
 	"math/rand"
 	"net/http"
 	"strconv"
 
 	"github.com/gorilla/mux"
 )
+
+type Movie struct {
+	ID       string    `json: "id"`
+	isbn     string    `json: "isbn"`
+	title    string    `json: "title"`
+	Director *Director `json: "director"`
+}
+
+type Director struct {
+	FirstName string `json: "firstName`
+	LastName  string `json: "lastName"`
+}
+
+var movies []Movie
 
 func main() {
 

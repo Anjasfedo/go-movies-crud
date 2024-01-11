f5a9d8e69b97cecabfb229d9ff8c308a7ce6aa9d
AnjasFedo <fedoaf98@gmail.com>
2024-01-11 22:45:51 +0700

    Define the Methods

--- main.go
+++ main.go
@@ -26,5 +26,20 @@ type Director struct {
 var movies []Movie
 
 func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting Server on Port 8000/n")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 
 }
